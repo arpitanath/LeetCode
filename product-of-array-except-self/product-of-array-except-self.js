9c412e11175b5722422c109b1bdb2f46b32b3105
Arpita Nath <61305520+arpitanath@users.noreply.github.com>
2021-12-12 16:40:21 +0530

    Time: 112 ms (85.26%), Space: 49.5 MB (82.02%) - LeetHub

---
+++ product-of-array-except-self/product-of-array-except-self.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    
+    let output=[];
+    
+    let left =1; 
+    
+    let right =1;
+    
+    for(let i=nums.length-1;i>=0;i--){
+        
+        output[i]=right;
+        right = right * nums[i]
+    }
+    
+    for(let j=0;j<nums.length;j++){
+        
+        output[j]*=left;
+        left=left*nums[j];
+    }
+    return output;
+};
